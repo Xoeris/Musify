d9e7acd611c38756a85c0a6406e9d34bb7ba736f
Acelbyte <marcelloemar@gmail.com>
2025-04-24 10:53:28 +0700

    external update

--- app/src/main/java/com/xoeris/android/musify/app/fragment/SettingsFragment.java
+++ app/src/main/java/com/xoeris/android/musify/app/fragment/SettingsFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.xoeris.android.musify.R;
+import com.xoeris.android.musify.app.activity.AboutSettingsActivity;
 import com.xoeris.android.musify.app.activity.DisplaySettingsActivity;
 import com.xoeris.android.musify.app.activity.SoundSettingsActivity;
 
@@ -17,8 +18,10 @@ public class SettingsFragment extends Fragment {
 
     private Intent intent;
     private Intent intent2;
+    private Intent intent3;
     private View displayCard;
     private View soundCard;
+    private View aboutCard;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -32,6 +35,11 @@ public class SettingsFragment extends Fragment {
         soundCard.setOnClickListener(v -> {
             intent2 = new Intent(getActivity(), SoundSettingsActivity.class);
             startActivity(intent2);
+        });
+        aboutCard = view.findViewById(R.id.aboutCard);
+        aboutCard.setOnClickListener(v -> {
+            intent3 = new Intent(getActivity(), AboutSettingsActivity.class);
+            startActivity(intent3);
         });
         return view;
     }
